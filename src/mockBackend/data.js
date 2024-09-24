0ec0d76ebdbba768e1f33cbbd4632c5d3539bd79
deijaparker <deijaparker1@outlook.com>
2024-09-23 19:54:41 -0700

    Implemented dynamic shopping app with React Hooks, featuring category-based item fetching and state management using useState and useEffect

---
+++ src/mockBackend/data.js
@@ -0,0 +1,7 @@
+export default {
+  "/categories": ["Shirts", "Pants", "Shoes", "Accessories"],
+  "/items?category=Shirts": ["T-Shirts", "Casual", "Formal"],
+  "/items?category=Pants": ["Long Pants", "Sweat Pants", "Shorts", "Swimwear"],
+  "/items?category=Shoes": ["Athletic", "Professional", "Casual", "Walking"],
+  "/items?category=Accessories": ["Hats", "Wallets", "Belts"],
+};
